e940f803b8f3f2cf4b60ab234472d85e98418fc9
connectshark <bobhus394@gmail.com>
2019-06-25 01:29:26 +0800

    首頁導覽列開始進行

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="UTF-8">
+<head>
+	<meta charset="UTF-8">
+	<meta name="keywords" content="菓籽戀冰所">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>菓籽戀冰所</title>
+	<link rel="icon" href="img/LOGO.png">
+	<link rel="stylesheet" href="css/reset.css">
+	<link rel="stylesheet" href="css/nav.css">
+	<link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+	<header id="header">
+		<div class="item_group">
+			<div class="nav_item">
+				<ul class="nav_list">
+					<li><a href="javascript:;" title="戀愛冰品">戀愛冰品</a></li>
+					<li><a href="javascript:;" title="冰棒客製">冰棒客製</a></li>
+					<li><a href="javascript:;" title="愛的留言">愛的留言</a></li>
+					<li><a href="javascript:;" title="冰品商城">冰品商城</a></li>
+				</ul>
+			</div>
+			<div class="logo_item">
+				<img src="img/LOGO.png" alt="菓籽戀冰所">
+			</div>
+			<div class="nav_item">
+				<ul class="nav_list">
+				<li><a href="javascript:;" title="體驗課程">體驗課程</a></li>
+				<li><a href="javascript:;" title="最新消息">最新消息</a></li>
+				<li><a href="javascript:;" title="關於園區">關於園區</a></li>
+				<li>
+					<ul>
+						<li><a href="javascript:;" title="會員">會員</a></li>
+						<li><a href="javascript:;" title="購物車">購物車</a></li>
+					</ul>
+				</li>
+				</ul>
+			</div>
+		</div>
+	</header>
+</body>
+</html>
