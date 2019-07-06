fb3ebc3be25d556b18c11ca713a21a83a649f007
connectshark <bobhus394@gmail.com>
2019-07-06 20:31:37 +0800

    留言板面修正

--- js/writemessage.js
+++ js/writemessage.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
     $('#submit').attr('disabled',true);
+    $('#userTitle').focus(open).blur(close);
+    $('#userContent').focus(open).blur(close);
+    $('#userPhrase').focus(open).blur(close);
 });
+function open() {
+	$(this).css('padding', '30px 20px');
+}
+function close(){
+	$(this).css('padding', '5px 20px');
+}
