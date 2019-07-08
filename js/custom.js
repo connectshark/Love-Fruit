9a1f390fb0439a59ffa25363589794e5db306c86
connectshark <bobhus394@gmail.com>
2019-07-08 19:17:25 +0800

    客製頁更新15%

--- js/custom.js
+++ js/custom.js
@@ -42,7 +42,27 @@ function nextChange() {
 	flowChange(nowFlow);
 	stepChange(nowFlow);
 }
+// 第一步換圖
+function imgChange(){
+	var src = $(this).children().children().attr('src');
+	var alt = $(this).children().children().attr('alt');
+	console.log(src);
+	$('#texture-main').attr({
+		src:src,
+		alt:alt,
+	});
+	$('.list-description').eq(0).text(alt);
+	$('#texture-min-img').attr('src',src);
+	$('#texture-min').css('backgroundColor','#f596aa');
+	$('.texture-select').children('figure').css('backgroundColor','#f596aa');
+	$(this).children('figure').css('backgroundColor','#db4d6d');
+}
+// 第二步換水果
+function fruiteChange() {
+	var src = $(this).children().children().attr('src');
+	var alt = $(this).children().children().attr('alt');
 
+}
 $(document).ready(function() {
 	$('#last').click(lastChange);
 	$('#next').click(nextChange);
@@ -50,4 +70,7 @@ $(document).ready(function() {
 	step = $('.step-item');
 	flowChange(nowFlow);
 	stepChange(nowFlow);
+	// 第一步
+	$('.texture-select').click(imgChange);
+	$('.fruite-item').click(fruiteChange);
 });
