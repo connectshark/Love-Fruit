1d3c7e40224672aea6fd1ce912f5d307530865fe
ROGETA <david19931028@outlook.com>
2019-07-12 12:05:22 +0800

    購物車正常流程

--- js/shop.js
+++ js/shop.js
@@ -1,3 +1,75 @@
+var storage = localStorage;
+function doFirst(){
+	if(storage['addItemList'] == null){
+		storage['addItemList'] = '';	//storage.setItem('addItemList','');
+	}
+
+	// 幫每個Add Cart建事件聆聽功能
+	var list = document.querySelectorAll('.shop-btn');		//list是陣列
+	for(var i=0; i<list.length; i++){
+		console.log(i);
+		list[i].addEventListener('click', function(){
+			// var teddyInfo = document.querySelector('#'+this.id+' input').value;
+			var teddyInfo = document.querySelector(`#${this.id} input`).value;
+			addItem(this.id, teddyInfo);
+		});
+	}
+}
+function addItem(itemId, itemValue){
+	// alert(itemId+' : '+itemValue);
+	var image = document.createElement('img');
+	image.src = 'imgs/' + itemValue.split('|')[1];
+
+	var title = document.createElement('span');
+	title.innerText = itemValue.split('|')[0];
+
+	var price = document.createElement('span');
+	price.innerText = itemValue.split('|')[2];
+
+	var newItem = document.getElementById('newItem');
+	
+	//先判斷此處是否已有物件，如果有要先刪除
+	while(newItem.childNodes.length >= 1){
+		newItem.removeChild(newItem.firstChild);
+	}
+
+	//再顯示新物件
+	newItem.appendChild(image);
+	newItem.appendChild(title);
+	newItem.appendChild(price);
+
+	//存入storage ?我看不懂 ?       
+	if(storage[itemId]){
+		alert('You have checked.');
+	}else{
+		storage[itemId] = itemValue;		//storage.setItem(itemId,itemValue);
+		storage['addItemList'] += itemId + ', ';
+	}
+
+	//計算購買數量和小計
+	var itemString = storage.getItem('addItemList');
+	var items = itemString.substr(0,itemString.length-2).split(', ');
+//	console.log(items);		//["A1001", "A1005", "A1006", "A1002"]
+	subtotal = 0 ;
+	for(var key in items){
+		var itemInfo = storage.getItem(items[key]);
+		var itemPrice = parseInt(itemInfo.split('|')[2]);
+		subtotal += itemPrice;
+	}
+	document.getElementById('itemCount').innerText = items.length;
+	document.getElementById('subtotal').innerText = subtotal;
+}
+window.addEventListener('load', doFirst);
+
+
+
+
+
+
+
+
+
+
 //按鈕壞顏色
 $(document).ready(function() {
 	$('#all').click(function(){
@@ -70,17 +142,6 @@ $(document).ready(function() {
 	});
 });
 
-
-//點擊愛心
-// $("#latest-collection-love").click(function(){                                 //click事件
-//     $(this).attr("src"," img/shop/collection-red.png");
-//     if ($("#latest-collection-love").attr() == attr("src"," img/shop/collection-red.png")){
-//          $("#latest-collection-love").click(function(){
-//         $(this).attr("src"," img/shop/collection-gray.png");
-//         })    
-//     }
-   
-// });
 //點擊愛心
 var love = document.getElementsByClassName("latest-collection-love");
 for(var i=0 ;i< love.length; i++){
@@ -99,22 +160,6 @@ function change(){
 	}
 	
 }
- // love[i].onclick=function(){
-    //     if(gray) {
-    //         $(this).attr("src","img/shop/collection-red.png");
-    //         gray = false;}
-    //         else{
-    //             $(this).attr("src","img/shop/collection-gray.png");
-    //             gray = true;
-    //         }
-    // })
-
-
-
-
-
-
-
 
 
 // 點擊愛心 js
