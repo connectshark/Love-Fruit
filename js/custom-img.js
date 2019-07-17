24309733ee29994b50a471c2e4f7d75dafa393a2
connectshark <bobhus394@gmail.com>
2019-07-17 18:05:43 +0800

    漸層放棄

--- js/custom-img.js
+++ js/custom-img.js
@@ -73,41 +73,41 @@
 	// ctx.fill();
 	// 第二個模型結束
 	// 第三個模型開始兔兔
-	ctx.beginPath();
-	ctx.lineWidth=3;
-	ctx.strokeStyle="rgba(0,0,0,0)";
-	ctx.moveTo(105,190);
-	ctx.quadraticCurveTo(18,130,42,66);
-	ctx.quadraticCurveTo(100,30,120,38);
-	ctx.quadraticCurveTo(148,45,180,142);
-	ctx.quadraticCurveTo(215,125,222,134);
-	ctx.quadraticCurveTo(260,35,310,39);
-	ctx.lineTo(330,39);
-	ctx.quadraticCurveTo(353,47,357,70);
-	ctx.quadraticCurveTo(360,110,286,178);
-	ctx.quadraticCurveTo(312,205,307,249);
-	ctx.quadraticCurveTo(326,265,304,294);
-	ctx.quadraticCurveTo(326,295,325,305);
-	ctx.quadraticCurveTo(346,306,344,330);
-	ctx.quadraticCurveTo(344,368,295,380);
-	ctx.lineTo(220,380);
-	ctx.lineTo(220,475);
-	ctx.quadraticCurveTo(195,500,170,475);
-	ctx.lineTo(170,380);
-	ctx.lineTo(80,380);
-	ctx.quadraticCurveTo(42,378,44,340);
-	ctx.quadraticCurveTo(53,312,77,300);
-	ctx.quadraticCurveTo(65,275,82,260);
-	ctx.quadraticCurveTo(70,225,105,190);
-	ctx.clip();
-	ctx.fillStyle="#FFD5B3";
-	ctx.fillRect(170,380,50,200);
-	var gradient = ctx.createLinearGradient(175,0,175,500);//漸層色起點&漸層色終點
-	gradient.addColorStop(0,'orange');
-	gradient.addColorStop(1,'red');
-	ctx.fillStyle=gradient;
-	ctx.fillRect(0,0,500,380);
-	ctx.fill();
+	// ctx.beginPath();
+	// ctx.lineWidth=3;
+	// ctx.strokeStyle="rgba(0,0,0,0)";
+	// ctx.moveTo(105,190);
+	// ctx.quadraticCurveTo(18,130,42,66);
+	// ctx.quadraticCurveTo(100,30,120,38);
+	// ctx.quadraticCurveTo(148,45,180,142);
+	// ctx.quadraticCurveTo(215,125,222,134);
+	// ctx.quadraticCurveTo(260,35,310,39);
+	// ctx.lineTo(330,39);
+	// ctx.quadraticCurveTo(353,47,357,70);
+	// ctx.quadraticCurveTo(360,110,286,178);
+	// ctx.quadraticCurveTo(312,205,307,249);
+	// ctx.quadraticCurveTo(326,265,304,294);
+	// ctx.quadraticCurveTo(326,295,325,305);
+	// ctx.quadraticCurveTo(346,306,344,330);
+	// ctx.quadraticCurveTo(344,368,295,380);
+	// ctx.lineTo(220,380);
+	// ctx.lineTo(220,475);
+	// ctx.quadraticCurveTo(195,500,170,475);
+	// ctx.lineTo(170,380);
+	// ctx.lineTo(80,380);
+	// ctx.quadraticCurveTo(42,378,44,340);
+	// ctx.quadraticCurveTo(53,312,77,300);
+	// ctx.quadraticCurveTo(65,275,82,260);
+	// ctx.quadraticCurveTo(70,225,105,190);
+	// ctx.clip();
+	// ctx.fillStyle="#FFD5B3";
+	// ctx.fillRect(170,380,50,200);
+	// var gradient = ctx.createLinearGradient(175,0,175,500);//漸層色起點&漸層色終點
+	// gradient.addColorStop(0,'orange');
+	// gradient.addColorStop(1,'red');
+	// ctx.fillStyle=gradient;
+	// ctx.fillRect(0,0,500,380);
+	// ctx.fill();
 
 	// 第三個模型結束
 
@@ -120,37 +120,37 @@
 
 	// 第四個模型開始火箭
 	// ctx.beginPath();
-	// ctx.lineWidth=3;
-	// ctx.strokeStyle="rgba(0,0,0,0)";
-	// ctx.moveTo(90,222);
-	// ctx.quadraticCurveTo(95,122,163,40);
-	// ctx.quadraticCurveTo(180,44,201,36);
-	// ctx.quadraticCurveTo(260,64,281,210);
-	// ctx.quadraticCurveTo(308,225,310,250);
-	// ctx.quadraticCurveTo(312,265,311,275);
-	// ctx.quadraticCurveTo(320,280,322,290);
-	// ctx.quadraticCurveTo(335,294,336,305);
-	// ctx.quadraticCurveTo(338,350,290,375);
-	// ctx.lineTo(220,375);
-	// ctx.lineTo(220,475);
-	// ctx.quadraticCurveTo(195,500,170,475);
-	// ctx.lineTo(170,375);
-	// ctx.lineTo(70,375);
-	// ctx.quadraticCurveTo(35,365,36,324);
-	// ctx.quadraticCurveTo(40,305,65,285);
-	// ctx.quadraticCurveTo(65,243,90,222);
-	// ctx.clip();
+	ctx.lineWidth=3;
+	ctx.strokeStyle="rgba(0,0,0,0)";
+	ctx.moveTo(90,222);
+	ctx.quadraticCurveTo(95,122,163,40);
+	ctx.quadraticCurveTo(180,44,201,36);
+	ctx.quadraticCurveTo(260,64,281,210);
+	ctx.quadraticCurveTo(308,225,310,250);
+	ctx.quadraticCurveTo(312,265,311,275);
+	ctx.quadraticCurveTo(320,280,322,290);
+	ctx.quadraticCurveTo(335,294,336,305);
+	ctx.quadraticCurveTo(338,350,290,375);
+	ctx.lineTo(220,375);
+	ctx.lineTo(220,475);
+	ctx.quadraticCurveTo(195,500,170,475);
+	ctx.lineTo(170,375);
+	ctx.lineTo(70,375);
+	ctx.quadraticCurveTo(35,365,36,324);
+	ctx.quadraticCurveTo(40,305,65,285);
+	ctx.quadraticCurveTo(65,243,90,222);
+	ctx.clip();
 
 
-	// ctx.beginPath();
-	// ctx.fillStyle="#FFD5B3";
-	// ctx.fillRect(170,375,50,150);
-	// var gradient = ctx.createLinearGradient(175,0,175,500);//漸層色起點&漸層色終點
-	// gradient.addColorStop(0,'orange');
-	// gradient.addColorStop(1,'red');
-	// ctx.fillStyle=gradient;
-	// ctx.fillRect(0,0,500,375);
-	// ctx.fill();
+	ctx.beginPath();
+	ctx.fillStyle="#FFD5B3";
+	ctx.fillRect(170,375,50,150);
+	var gradient = ctx.createLinearGradient(175,0,175,500);//漸層色起點&漸層色終點
+	gradient.addColorStop(0,'orange');
+	gradient.addColorStop(1,'red');
+	ctx.fillStyle=gradient;
+	ctx.fillRect(0,0,500,375);
+	ctx.fill();
 
 	// 第四個模型結束
 
@@ -168,7 +168,7 @@
 
 
 	var texturePic = new Image();
-	texturePic.src = "../img/custom/texture3.png";
+	texturePic.src = "../img/custom/texture4.png";
 	texturePic.addEventListener('load',function () {
 		ctx.drawImage(texturePic,0,0,texturePic.width,texturePic.height);
 	});
@@ -179,7 +179,7 @@
 	// fruiteslice.addEventListener('load',function() {
 	// 	ctx.drawImage(fruiteslice,175,0,fruiteslice.width,fruiteslice.height);
 	// })
-	// bg = canvas.toDataURL("image/png");
+	bg = canvas.toDataURL("image/png");
 	// ctx.clearRect(0, 0, canvas.width, canvas.height);
 	// console.log(bg);
 	// var bgi = new Image();
