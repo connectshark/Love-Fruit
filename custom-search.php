515d8bf72baa5d0a8d78c8ef423c634301962f5a
connectshark <bobhus394@gmail.com>
2019-07-18 12:03:06 +0800

    客製

--- custom-search.php
+++ custom-search.php
@@ -1,4 +1,5 @@
 <?php 
+$errMsg="";
 try {
     $dsn="mysql:host=localhost;port=3306;dbname=dd101g3;charset=utf8";
     $user = "root";
@@ -23,7 +24,8 @@ try {
     
 } catch (PDOException $e) {
     $errMsg .= "錯誤訊息:". $e->getMessage() ."<br>";
-    $errMsg .= "行數:". $e->getLine()."<br>";
+	$errMsg .= "行數:". $e->getLine()."<br>";
+	echo $errMsg;
 }
 
  ?>
