48f7d1933b5045e30fc415d85d5b420689b774b6
shihpeihuang <shihpei.huang@gmail.com>
2019-07-18 21:17:24 +0800

    會員中心剩下 訂單詳情 + 取消收藏

--- ajaxProfileChange.php
+++ ajaxProfileChange.php
@@ -2,14 +2,14 @@
 $errMsg = "";
 try {
     require_once("connect-dd101g3.php");
-    $sql = "update member set mem_name=:name, mem_psw=:psw, email=:email, address=:address, phone=:phone where mem_no = :mem_id";
+    $sql = "update member set mem_name=:name, mem_psw=:psw, email=:email, address=:address, phone=:phone where mem_no = :mem_no";
     $profileChange = $pdo->prepare($sql);
     $profileChange->bindValue(":name", $_POST["mem_name"]);
     $profileChange->bindValue(":psw", $_POST["mem_psw"]);
     $profileChange->bindValue(":email", $_POST["email"]);
     $profileChange->bindValue(":address", $_POST["address"]);
     $profileChange->bindValue(":phone", $_POST["phone"]);
-    $profileChange->bindValue(":mem_id", 1);
+    $profileChange->bindValue(":mem_no", 1);
     $profileChange->execute();
     $profileChangeRow = $profileChange->fetch(PDO::FETCH_ASSOC);
 
