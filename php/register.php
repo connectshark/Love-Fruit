1f4ac06e4d6a7f98c1efeae56104925c4438a099
薩柏竣 <harry51805@gmail.com>
2019-07-21 16:41:10 +0800

    update

--- php/register.php
+++ php/register.php
@@ -1,14 +1,15 @@
 <?php
 // 註冊 register
 try {
-    if ($_REQUEST["mem_id"] != '' || $_REQUEST["mem_email"] != '' || $_REQUEST["mem_psw"] != '') {
+    if ($_REQUEST["mem_id"] != '' || $_REQUEST["email"] != '' || $_REQUEST["mem_psw"] != '') {
         require_once("connect-LoveFruitIce.php");
-        $sql = "insert into member (mem_name,mem_id,mem_email,mem_psw) values (:mem_name,:mem_id,:mem_email,:mem_psw)";
+        $sql = "insert into member (mem_name,mem_id,email,mem_psw,mem_pic) values (:mem_name,:mem_id,:email,:mem_psw,:mem_pic)";
         $member = $pdo->prepare($sql);
         $member->bindValue(":mem_name", $_REQUEST["mem_name"]);
         $member->bindValue(":mem_id", $_REQUEST["mem_id"]);
-        $member->bindValue(":mem_email", $_REQUEST["mem_email"]);
         $member->bindValue(":mem_psw", $_REQUEST["mem_psw"]);
+        $member->bindValue(":email", $_REQUEST["email"]);
+        $member->bindValue(":mem_pic", $_REQUEST["mem_pic"]);
         $member->execute();
     }
 } catch (PDOException $e) {
